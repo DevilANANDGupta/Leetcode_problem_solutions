58c3f361abb05e4d8458ee4b362478302643c384
anand gupta <90339262+DevilANANDGupta@users.noreply.github.com>
2022-08-06 12:32:46 +0530

    Create reverse_linked_list.py

---
+++ reverse_linked_list.py
@@ -0,0 +1,12 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        next = None
+        while head:
+            head.next, head, next = next, head.next, head
+        return next 
+        
