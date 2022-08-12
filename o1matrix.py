5845e20397ea5936f49ecb32682b33308994cabf
anand gupta <90339262+DevilANANDGupta@users.noreply.github.com>
2022-08-12 22:39:28 +0530

    Update o1matrix.py

--- o1matrix.py
+++ o1matrix.py
@@ -1,1 +1,24 @@
-class solution:
+class Solution(object):
+    def updateMatrix(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        for i in xrange(len(matrix)):
+            for j in xrange(len(matrix[i])):
+                if not matrix[i][j]:
+                    continue
+                matrix[i][j] = float("inf")
+                if i > 0:
+                    matrix[i][j] = min(matrix[i][j], matrix[i-1][j]+1)
+                if j > 0:
+                    matrix[i][j] = min(matrix[i][j], matrix[i][j-1]+1)
+        for i in reversed(xrange(len(matrix))):
+            for j in reversed(xrange(len(matrix[i]))):
+                if not matrix[i][j]:
+                    continue
+                if i < len(matrix)-1:
+                    matrix[i][j] = min(matrix[i][j], matrix[i+1][j]+1)
+                if j < len(matrix[i])-1:
+                    matrix[i][j] = min(matrix[i][j], matrix[i][j+1]+1)
+        return matrix
