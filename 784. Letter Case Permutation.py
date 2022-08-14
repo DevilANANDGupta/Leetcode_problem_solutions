026bad9b7c0ddbf2b2303de3d509a8d462d28aaf
anand gupta <90339262+DevilANANDGupta@users.noreply.github.com>
2022-08-14 20:34:18 +0530

    Create 784. Letter Case Permutation.py

---
+++ 784. Letter Case Permutation.py
@@ -0,0 +1,37 @@
+'''Given a string s, you can transform every letter individually to be lowercase or uppercase to create another string.
+
+Return a list of all possible strings we could create. Return the output in any order.
+
+ 
+
+Example 1:
+
+Input: s = "a1b2"
+Output: ["a1b2","a1B2","A1b2","A1B2"]
+Example 2:
+
+Input: s = "3z4"
+Output: ["3z4","3Z4"]
+ 
+
+Constraints:
+
+1 <= s.length <= 12
+s consists of lowercase English letters, uppercase English letters, and digits.'''
+class Solution(object):
+    def letterCasePermutation(self, S):
+        """
+        :type S: str
+        :rtype: List[str]
+        """
+        result = [[]]
+        for c in S:
+            if c.isalpha():
+                for i in xrange(len(result)):
+                    result.append(result[i][:])
+                    result[i].append(c.lower())
+                    result[-1].append(c.upper())
+            else:
+                for s in result:
+                    s.append(c)
+        return map("".join, result)
