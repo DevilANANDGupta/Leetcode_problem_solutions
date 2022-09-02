019a6ffe2a6d646bb7180fb36109ceb175b0d0f4
anand gupta <90339262+DevilANANDGupta@users.noreply.github.com>
2022-09-02 23:19:16 +0530

    Update 54.Spiral Matrix.py

--- 54.Spiral Matrix.py
+++ 54.Spiral Matrix.py
@@ -26,12 +26,16 @@ class Solution:
         start_row,end_row,start_col,end_col = 0,len(matrix),0,len(matrix[0])
         output=[]
         while start_row<end_row or start_col<end_col:
-            output.extend([matrix[start_row][i] for i in range(start_col,end_col)])
+            if start_row<end_row:
+                output.extend([matrix[start_row][i] for i in range(start_col,end_col)])
             start_row +=1
-            output.extend([matrix[i][end_col-1] for i in range(start_row, end_row)])
+            if start_col<end_col:
+                output.extend([matrix[i][end_col-1] for i in range(start_row, end_row)])
             end_col -=1
-            output.extend([matrix[end_row-1][i] for i in range(end_col-1,start_col-1,-1)])
+            if start_row<end_row:
+                output.extend([matrix[end_row-1][i] for i in range(end_col-1,start_col-1,-1)])
             end_row -=1
-            output.extend([matrix[i][start_col] for i in range(end_row-1,start_row-1,-1)])
+            if start_col<end_col:
+                output.extend([matrix[i][start_col] for i in range(end_row-1,start_row-1,-1)])
             start_col +=1
         return output
