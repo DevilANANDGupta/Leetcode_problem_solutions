848ae864b8916c8b8dbcd298cb6f6aed597c106b
anand gupta <90339262+DevilANANDGupta@users.noreply.github.com>
2022-09-05 19:59:57 +0530

    Create 429. N-ary Tree Level Order Traversal.py

---
+++ 429. N-ary Tree Level Order Traversal.py
@@ -0,0 +1,40 @@
+'''Given an n-ary tree, return the level order traversal of its nodes' values.
+
+Nary-Tree input serialization is represented in their level order traversal, each group of children is separated by the null value (See examples).
+
+ 
+
+Example 1:
+
+
+
+Input: root = [1,null,3,2,4,null,5,6]
+Output: [[1],[3,2,4],[5,6]]
+Example 2:
+
+
+
+Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+Output: [[1],[2,3,4,5],[6,7,8,9,10],[11,12,13],[14]]'''
+
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        l = defaultdict(list)
+        def dfs(node, level):
+            if not node:
+                return 
+            l[level].append(node.val)
+            for c in node.children:
+                dfs(c, level+1)
+        dfs(root, 0)
+        return [lst for k,lst in sorted(l.items())]
+        
