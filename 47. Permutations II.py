d2e89c5635a7b5875204be9acf6ba585d6bae900
anand gupta <90339262+DevilANANDGupta@users.noreply.github.com>
2022-09-17 23:29:09 +0530

    Create 47. Permutations II.py

---
+++ 47. Permutations II.py
@@ -0,0 +1,37 @@
+'''Given a collection of numbers, nums, that might contain duplicates, return all possible unique permutations in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,2]
+Output:
+[[1,1,2],
+ [1,2,1],
+ [2,1,1]]
+Example 2:
+
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]'''
+
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        res = [] 
+        perm = []
+        count = { n:0 for n in nums}
+        for n in nums:
+            count[n] += 1
+        def dfs():
+            if len(perm) == len(nums):
+                res.append(perm.copy())
+                return
+            for n in count:
+                if count[n] > 0:
+                    perm.append(n)
+                    count[n] -= 1
+                    dfs()
+                    count[n] += 1
+                    perm.pop()
+        dfs()
+        return res
+        
