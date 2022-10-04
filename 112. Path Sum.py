9ab36e4d64c5879c61265bcebe854c9f78cf1c64
anand gupta <90339262+DevilANANDGupta@users.noreply.github.com>
2022-10-04 22:56:55 +0530

    Create 112. Path Sum.py

---
+++ 112. Path Sum.py
@@ -0,0 +1,27 @@
+'''Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+
+A leaf is a node with no children.
+
+ 
+
+Example 1:
+
+
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+Output: true
+Explanation: The root-to-leaf path with the target sum is shown.'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if not root:
+            return False
+        targetSum-=root.val
+        if not root.left and not root.right:
+            return targetSum==0
+        return self.hasPathSum(root.left,targetSum) or self.hasPathSum(root.right,targetSum)
