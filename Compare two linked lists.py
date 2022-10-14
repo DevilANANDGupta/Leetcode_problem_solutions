06c53940285c0283f59280cdf84ecb70439fffc5
anand gupta <90339262+DevilANANDGupta@users.noreply.github.com>
2022-10-14 16:52:34 +0530

    Create Compare two linked lists.py

---
+++ Compare two linked lists.py
@@ -0,0 +1,183 @@
+'''You’re given the pointer to the head nodes of two linked lists. Compare the data in the nodes of the linked lists to check if they are equal. If all data attributes are equal and the lists are the same length, return . Otherwise, return .
+
+Example
+
+
+
+The two lists have equal data attributes for the first  nodes.  is longer, though, so the lists are not equal. Return .
+
+Function Description
+
+Complete the compare_lists function in the editor below.
+
+compare_lists has the following parameters:
+
+SinglyLinkedListNode llist1: a reference to the head of a list
+SinglyLinkedListNode llist2: a reference to the head of a list
+Returns
+
+int: return 1 if the lists are equal, or 0 otherwise
+Input Format
+
+The first line contains an integer , the number of test cases.
+
+Each of the test cases has the following format:
+The first line contains an integer , the number of nodes in the first linked list.
+Each of the next  lines contains an integer, each a value for a data attribute.
+The next line contains an integer , the number of nodes in the second linked list.
+Each of the next  lines contains an integer, each a value for a data attribute.
+
+Constraints
+
+Output Format
+
+Compare the two linked lists and return 1 if the lists are equal. Otherwise, return 0. Do NOT print anything to stdout/console.
+
+The output is handled by the code in the editor and it is as follows:
+
+For each test case, in a new line, print  if the two lists are equal, else print .
+
+Sample Input
+
+2
+2
+1
+2
+1
+1
+2
+1
+2
+2
+1
+2
+Sample Output
+
+0
+1
+Explanation
+
+There are  test cases, each with a pair of linked lists.
+
+In the first case, linked lists are: 1 -> 2 -> NULL and 1 -> NULL
+
+In the second case, linked lists are: 1 -> 2 -> NULL and 1 -> 2 -> NULL'''
+
+
+#!/bin/python3
+
+import os
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the compare_lists function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+import os
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+
+def compare_lists(headA, headB):
+  if headA is None:
+    return 1 if headB is None else 0
+  if headB is None:
+    return 0
+  if headA.data != headB.data:
+    return 0
+  else:
+    return compare_lists(headA.next, headB.next)
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    tests = int(input())
+
+    for tests_itr in range(tests):
+        llist1_count = int(input())
+
+        llist1 = SinglyLinkedList()
+
+        for _ in range(llist1_count):
+            llist1_item = int(input())
+            llist1.insert_node(llist1_item)
+            
+        llist2_count = int(input())
+
+        llist2 = SinglyLinkedList()
+
+        for _ in range(llist2_count):
+            llist2_item = int(input())
+            llist2.insert_node(llist2_item)
+
+        result = compare_lists(llist1.head, llist2.head)
+
+        fptr.write(str(int(result)) + '\n')
+
+    fptr.close()
