40bede7e7b13e3095f1a6a8d1a19f96308fdd274
anand gupta <90339262+DevilANANDGupta@users.noreply.github.com>
2023-02-04 08:49:02 +0530

    Create 567. Permutation in String.py

---
+++ 567. Permutation in String.py
@@ -0,0 +1,11 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        s1_counter = collections.Counter(s1)
+        
+        s1_len = len(s1)
+        
+        for i in range(len(s2) - s1_len + 1):
+            if collections.Counter(s2[i:i+s1_len]) == s1_counter:
+                return True
+            
+        return False
