fcc1d08af2f4f2adf7df0f93a2c6f5c6c3912964
anand gupta <90339262+DevilANANDGupta@users.noreply.github.com>
2023-02-27 10:19:37 +0530

    Create 427. Construct Quad Tree.py

---
+++ 427. Construct Quad Tree.py
@@ -0,0 +1,24 @@
+class Solution:
+    def construct(self, grid: List[List[int]]) -> 'Node':
+        n = len(grid)
+        if n == 0:
+            return None
+        
+        def build_quadtree(x1, y1, x2, y2):
+            if x1 == x2 and y1 == y2:
+                return Node(grid[x1][y1], True, None, None, None, None)
+            
+            mid_x, mid_y = (x1 + x2) // 2, (y1 + y2) // 2
+            topLeft = build_quadtree(x1, y1, mid_x, mid_y)
+            topRight = build_quadtree(x1, mid_y + 1, mid_x, y2)
+            bottomLeft = build_quadtree(mid_x + 1, y1, x2, mid_y)
+            bottomRight = build_quadtree(mid_x + 1, mid_y + 1, x2, y2)
+            
+            # Check if all children are leaves with the same value
+            if (topLeft.isLeaf and topRight.isLeaf and bottomLeft.isLeaf and bottomRight.isLeaf and
+                topLeft.val == topRight.val == bottomLeft.val == bottomRight.val):
+                return Node(topLeft.val, True, None, None, None, None)
+            else:
+                return Node(None, False, topLeft, topRight, bottomLeft, bottomRight)
+        
+        return build_quadtree(0, 0, n - 1, n - 1)
