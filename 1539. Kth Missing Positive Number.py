6e8613588f67dbca1ed4294e1c4a8f4f0085c304
anand gupta <90339262+DevilANANDGupta@users.noreply.github.com>
2023-03-06 19:44:38 +0530

    Create 1539. Kth Missing Positive Number.py

---
+++ 1539. Kth Missing Positive Number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findKthPositive(self, arr: List[int], k: int) -> int:
+    # def findKthPositive(self, arr: List[int], k: int) -> int:
+        lo, hi = 0, len(arr)
+        while lo < hi:
+            mid = lo + (hi - lo) // 2
+            if arr[mid] - mid > k: hi = mid
+            else: lo = mid + 1
+        return lo + k
